854e8a56bded987820d71ba6355806e9c249fbfa
themodernlogicgroup-ofc <129704108+themodernlogicgroup-ofc@users.noreply.github.com>
2023-09-02 12:19:04 -0700

    Create mainapps.ps1

---
+++ mainapps.ps1
@@ -0,0 +1,30 @@
+cChocoPackageInstaller microsoft-teams.install
+{
+   Name     = "microsoft-teams.install"
+   Version  = "1.6.0.22378"
+   Source   = "https://github.com/themodernlogicgroup-ofc/chocolatety-installer"
+}
+cChocoPackageInstaller python
+{
+   Name     = "python"
+   Version  = "3.11.5"
+   Source   = "https://github.com/themodernlogicgroup-ofc/chocolatety-installer"
+}
+cChocoPackageInstaller 7zip.install
+{
+   Name     = "7zip.install"
+   Version  = "23.1.0"
+   Source   = "https://github.com/themodernlogicgroup-ofc/chocolatety-installer"
+}
+cChocoPackageInstaller googlechrome
+{
+   Name     = "googlechrome"
+   Version  = "116.0.5845.141"
+   Source   = "https://github.com/themodernlogicgroup-ofc/chocolatety-installer"
+}
+cChocoPackageInstaller adobereader
+{
+   Name     = "adobereader"
+   Version  = "2023.3.20284"
+   Source   = "https://github.com/themodernlogicgroup-ofc/chocolatety-installer"
+}
